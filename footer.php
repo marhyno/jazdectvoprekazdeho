ae3132e69c744d0427f685b74121cffa6c8d1a5f
Štefan Marcin <stefan.marcin74@gmail.com>
2020-07-30 00:25:33 +0200

    removed footer payed line

--- footer.php
+++ footer.php
@@ -56,7 +56,7 @@
 					<div class="footer-bottom row">
 						<p class="footer-text m-0 col-lg-6 col-md-12">
 							<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                            Copyright &copy;<script>document.write(new Date().getFullYear());</script> Všetky práva vyhradené | Designed <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                            Copyright &copy;<script>document.write(new Date().getFullYear());</script> Všetky práva vyhradené
                             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 						<br>
 						<a href="/ochrana-udajov.php">Ochrana osobných údajov</a>
