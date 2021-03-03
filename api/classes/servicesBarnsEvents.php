d6ef63cde6b648a04a38f925914efa2b277722da
Štefan Marcin <stefan.marcin74@gmail.com>
2021-03-03 01:26:45 +0100

    removed union defauklt from galleries

--- api/classes/servicesBarnsEvents.php
+++ api/classes/servicesBarnsEvents.php
@@ -205,7 +205,7 @@ class servicesBarnsEvents{
                 SUBSTRING(`descriptionOfService`, 1, 200) as descriptionOfService,
                 price FROM services WHERE barnId = :ID", array('ID' => $barnId));
         //galeries
-        $barnDetails['gallery'] = getData("SELECT ID,barnId, imageLink FROM barnGalleries WHERE barnId = :ID UNION SELECT 'default',ID, barnImage FROM barns WHERE ID = :ID", array('ID' => $barnId));
+        $barnDetails['gallery'] = getData("SELECT ID,barnId, imageLink FROM barnGalleries WHERE barnId = :ID", array('ID' => $barnId));
         //events
         $barnDetails['events'] = getData("SELECT events.ID,
                 barnId,
